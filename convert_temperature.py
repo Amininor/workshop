fbcd66aebfaac488fed001ea7a8ca690dfdd9da5
Amanda Martinez Reyes <a.martinezreyes@tudeldt.nl>
2023-05-25 14:22:28 +0200

    added a description to the function

--- convert_temperature.py
+++ convert_temperature.py
@@ -1,4 +1,12 @@
 def fah_to_cel(temp_fahrenheit):
+	"""
+	Converts temperature from fahrenheit to celsius
+	Parameters:
+	temp_fahrenheit
+
+	Returns:
+	temp_celsius
+	"""
 	temp_celsius = (temp_fahrenheit -32) * 5/9
 	return temp_celsius
 
